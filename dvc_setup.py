c3e75940f9d3e2be974e27bad88bda41bfe6d955
Joud <joudabihaidar2@gmail.com>
2024-07-29 14:26:24 +0300

    testing

--- dvc_setup.py
+++ dvc_setup.py
@@ -2,18 +2,24 @@ import os
 import subprocess
 
 def run_command(command):
-    """Run a system command and print the output."""
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    """
+    This function executes a system command and print its output.
+    """
+    result=subprocess.run(command, shell=True, capture_output=True, text=True)
+    # stdout: standard output
+    # stderr: standard error
     if result.stdout:
         print(result.stdout)
     if result.stderr:
         print(result.stderr)
     return result
 
+
 def initialize_git_repo(repo_path):
     """Initialize a Git repository."""
     os.chdir(repo_path)
     run_command('git init')
+    run_command('dvc init')
     run_command('git add .dvc .gitignore')
     run_command('git commit -m "Initialize DVC project"')
 
@@ -32,24 +38,23 @@ def track_and_push_data(repo_path, data_file):
     run_command('dvc push')
 
 def main():
-    # My paths and parameters
-    repo_path = r'C:\Users\Legion\Desktop\my_dvc_project'
+    repo_path = r"C:\Users\Legion\Desktop\dvc_setup"
     data_file = 'data/data.csv'
     remote_name = 'myremote'
     drive_folder_id = '14OTN7AaJJlXYzOuRuEjeCAldEEds4ZWN'
-    github_repo_url = 'https://github.com/joudabihaidar/my-dvc-project.git'
+    github_repo_url = 'https://github.com/joudabihaidar/data-version-control-setup.git'
 
-    # # Initializing a Git repository 
-    # initialize_git_repo(repo_path)
+    # Initializing a Git repository 
+    initialize_git_repo(repo_path)
 
-    # # Configuring DVC remote storage with Google Drive
-    # configure_dvc_remote(repo_path, remote_name, drive_folder_id)
+    # Configuring DVC remote storage with Google Drive
+    configure_dvc_remote(repo_path, remote_name, drive_folder_id)
 
     # Track data file with DVC and push to remote storage
     track_and_push_data(repo_path, data_file)
 
-    # # Push changes to GitHub
-    # run_command(f'git remote add origin {github_repo_url}')
+    # Push changes to GitHub
+    run_command(f'git remote add origin {github_repo_url}')
     run_command('git push -u origin master')
 
 if __name__ == "__main__":
